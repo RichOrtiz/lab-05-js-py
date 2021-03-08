56bd1f9e1ea372f7414c6735c64f45b897e8806c
Richard Ortiz <ortizrichard2@gmail.com>
2021-03-08 17:01:16 -0500

    Added the python version of madlibs and moved the placement of the adverb for the html version

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+noun = input("Enter a noun: ")
+adj = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+verb = input("Enter a verb: ")
+
+print("On a tropical " + str(noun) + " \n Underneath a " + str(adj) + " lava moon. \n " + str(verb) + " with the hula dancers, \n"+ str(adverb) + " Askin' questions cause' they got all the answers.")
